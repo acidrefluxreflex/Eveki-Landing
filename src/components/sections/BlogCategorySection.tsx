57259abca661ffc20ba11b209f444cf62796545b
kabuki <tanuki5sanuki3@icloud.com>
2023-05-26 19:25:37 +0900

    d

--- src/components/sections/BlogCategorySection.tsx
+++ src/components/sections/BlogCategorySection.tsx
@@ -1,46 +1,26 @@
-
-import {
-    getCategory
-  } from "../../libs/microcms";
+import { getCategory } from "../../libs/microcms";
 import Link from "next/link";
-  
-  export async function generateStaticParams() {
-    const { contents } = await getCategory();
-  
-    const paths = contents.map((category) => {
-      return {
-        categoryId: category.id,
-      };
-    });
-  
-    return paths;
+
+
+
+export default async function BlogCategorySection() {
+  const { contents } = await getCategory();
+
+  if (!contents || contents.length === 0) {
+    return <h1 className="mt-8 text-center">No contents</h1>;
   }
-  
-  export default async function BlogCategorySection() {
-  
-  
-    const { contents } = await getCategory();
-  
-   
-  
-   
-  
-    if (!contents || contents.length === 0) {
-      return <h1 className="mt-8 text-center">No contents</h1>;
-    }
-  
-    return (
-      <div className="mx-auto max-w-2xl py-8">
-        <ul className="list-disc pl-8">
-          {contents.map((post) => {
-            return (
-              <div key={post.id} className="">
-             <Link  href={`/blog/category/${post.id}`}>{post.name}</Link>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-  
+
+  return (
+    <div className="mx-auto max-w-2xl py-8">
+      <ul className="list-disc pl-8">
+        {contents.map((post) => {
+          return (
+            <div key={post.id} className="">
+              <Link href={`/blog/category/${post.id}`}>{post.name}</Link>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
