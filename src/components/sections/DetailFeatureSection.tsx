62e6a5fb81518314b0a1beb2601704d049c5d752
kabuki <tanuki5sanuki3@icloud.com>
2023-05-26 16:20:57 +0900

    ss

--- src/components/sections/DetailFeatureSection.tsx
+++ src/components/sections/DetailFeatureSection.tsx
@@ -5,21 +5,21 @@ const DetailFeatureSection: React.FC = () => {
   return (
     <div className="my-8 flex justify-center">
       <Card
-        imageSrc="/path/to/image.jpg"
+        imageSrc="/images/Imaginary-friend-amico.svg"
         title="キャラクター"
         description="お嬢様言葉や関西弁など、話し方として様々なキャラを選択できます。"
         buttonText="ボタン"
         link="/"
       />
       <Card
-        imageSrc="/path/to/image.jpg"
+        imageSrc="/images/Messaging-fun-amico.svg"
         title="グループチャット"
         description="複数のAIを交えて会話することのできるグループチャットが可能です。"
         buttonText="ボタン"
         link="/"
       />
       <Card
-        imageSrc="/path/to/top.jpg"
+        imageSrc="/images/Critical-thinking-amico.svg"
         title="記憶の保持"
         description="会話した内容をAIに記憶させて、その前提で会話を再開できます。"
         buttonText="ボタン"
