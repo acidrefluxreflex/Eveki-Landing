87f598dc027712c4220e5332d8cbfe2bcb38942d
kabuki <tanuki5sanuki3@icloud.com>
2023-05-29 15:33:47 +0900

    koko

--- src/components/blog/SideBar.tsx
+++ src/components/blog/SideBar.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 
 const SideElement: React.FC = () => {
   return (
-    <div className="w-300px">
-        <Image src="/images/appBanner.webp" height={300} width={300} alt="Eveki 会話AI" />
+    <div className="w-300px gap-y-8">
+        <Image className="rounded-lg   my-8" src="/images/appBanner.webp" height={300} width={300} alt="Eveki 会話AI" />
+        <Image className="rounded-lg my-8" src="/images/noteBanner.webp" height={90} width={300} alt="Eveki 会話AI" />
+        <a href="https://www.instagram.com/eveki.ai/">
+        <Image className="rounded-lg my-8" src="/images/instaBanner.webp" height={90} width={300} alt="Eveki 会話AI" />
+        </a>
       {/* Your content here */}
     </div>
   );
